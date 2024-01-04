6f9a766cc01c2fcae0dd4ce49a3f0b845d435912
Mateusz42 <Mateusz.4007@wp.pl>
2024-01-05 00:22:17 +0100

    Add new files

--- src/index.js
+++ src/index.js
@@ -6,24 +6,24 @@ const searchBox = document.querySelector('#search-box');
 
 const handleSearch = debounce(async () => {
   const searchTerm = searchBox.value.trim();
-  
+
   if (searchTerm === '') {
    return;
   }
 
   try {
     const countries = await fetchCountries(searchTerm);
-    
+
     if (countries.length === 0) {
       Notiflix.Notify.failure('Oops, there is no country with that name');
-  
+
     } else if (countries.length > 10) {
       Notiflix.Notify.failure('Too many matches found. Please enter a more specific name.');
-    
+
     } else if (countries.length >= 2 && countries.length <= 10) {
-  
+
     } else {
-  
+
     }
   } catch (error) {
     Notiflix.Notify.failure(error.message);
