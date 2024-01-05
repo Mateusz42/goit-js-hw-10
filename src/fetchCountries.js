955df42865f52a955da762173599398fa4d29b69
Mateusz42 <Mateusz.4007@wp.pl>
2024-01-06 00:41:17 +0100

    Add new files

--- src/fetchCountries.js
+++ src/fetchCountries.js
@@ -1,15 +1,16 @@
 import Notiflix from 'notiflix';
-
 export async function fetchCountries(name) {
+  const baseUrl = 'https://restcountries.com/v3.1/name/';
+  const queryParams = '?fields=name,official,capital,population,flags,lnguages';
   try {
-    const response = await fetch(`https://restcountries.com/v3.1/name/${name}?fields=name.official,flags.svg,capital,population,languages`);
-    if (!response.ok) {
-      throw new Error('Country not found');
+      const response = await fetch(`${baseUrl}${name}${queryParams}`);
+      if (!response.ok) {
+          throw new Error('Country not found');
+      }
+      const countries = await response.json();
+      return countries;
+    } catch (error) {
+      Notiflix.Notify.failure('Oops, there is no country with that name');
+      throw new Error('Error fetching countries');
     }
-    const countries = await response.json();
-    return countries;
-  } catch (error) {
-    Notiflix.Notify.failure('Oops, there is no country with that name');
-    throw new Error('Error fetching countries');
   }
-}
